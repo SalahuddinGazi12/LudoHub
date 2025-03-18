53279fe6686f9bed832e6d92300030e4fc938759
Salahudddin12 <sgazifahim786@gmail.com>
2025-03-18 15:56:33 +0600

    dd

--- Assets/Scripts/Homes/LudoHome.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LudoHome : MonoBehaviour
-{
-    public RollingDice rollingDice;
-    public PlayerPiece[] playerPieces;
-}
