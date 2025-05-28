12a4ed305abca6074791ef94ed215f9b575a84c7
Salahudddin12 <sgazifahim786@gmail.com>
2025-05-28 12:48:08 +0600

    Chat Message Remove

--- Assets/Scripts/Test/ChatMessage.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public abstract class ChatMessage
-{
-    public string Sender { get; protected set; }
-    public DateTime Timestamp { get; } = DateTime.Now;
-    public abstract string FormatMessage();
-}
--- Assets/Scripts/Test/NetworkMessag.cs
+++
@@ -1,8 +0,0 @@
-[System.Serializable]
-public class NetworkMessage
-{
-    public string type;
-    public string sender;
-    public string content;
-    public string playerId;
-}
